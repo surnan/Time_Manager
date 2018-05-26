22c0c0e0521f9b52d97d911110b51c4ccbe67308
surnan <forsuresh@yandex.com>
2018-05-26 16:33:06 -0400

    start successfully without storyboard

--- Time_Manager/Time_Manager/ViewController.swift
+++ Time_Manager/Time_Manager/ViewController.swift
@@ -8,18 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TasksListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.yellow
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
