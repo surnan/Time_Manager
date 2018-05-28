4e8a063037e6019711d85ec56e3cad49ff90b83e
surnan <forsuresh@yandex.com>
2018-05-28 18:10:43 -0400

    CoreDataManager Singleton created in CoreDataManager.swift

---
+++ Time_Manager/Time_Manager/CoreData/CoreDataManager.swift
@@ -0,0 +1,26 @@
+//
+//  CoreDataManager.swift
+//  Time_Manager
+//
+//  Created by admin on 5/28/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+struct CoreDataManager {
+    static let shared = CoreDataManager()
+    
+    let persistentContainer: NSPersistentContainer = {
+        let myContainer = NSPersistentContainer(name: "CoreDataFile")
+        myContainer.loadPersistentStores { (store, err) in
+            if let err = err {
+                fatalError("Loading of store failed: \(err)")
+            }
+        }
+        let context = myContainer.viewContext
+        return myContainer
+    }()
+}
