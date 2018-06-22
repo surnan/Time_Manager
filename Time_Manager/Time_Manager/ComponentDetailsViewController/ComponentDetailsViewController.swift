5647be2cffb4f95ad48188cb2eacc9978f1b98bb
surnan <forsuresh@yandex.com>
2018-06-21 23:38:10 -0400

    UI test on componentdetailsviewcontroller

--- Time_Manager/Time_Manager/ComponentDetailsViewController/ComponentDetailsViewController.swift
+++ Time_Manager/Time_Manager/ComponentDetailsViewController/ComponentDetailsViewController.swift
@@ -11,82 +11,50 @@ import AVFoundation
 
 //class ComponentDetailsViewController: UIViewController {
 
-    class ComponentDetailsViewController: UIViewController {
-    
-    /*
-     let newVC = CreateComponentViewController()
-     newVC.currentComponentItem = components[indexPath.row]
-     newVC.parentTask = currentTask
-     newVC.delegate = self
-     navigationController?.pushViewController(newVC, animated: true)
-     */
-    
+class ComponentDetailsViewController: UIViewController {
     
     //MARK:- Parameters passed in
     var myDelegate: manipulatingComponentsListViewController? //passed-in
     var parentTask: TaskItem?   //passed-in
     var currentComponentItem: ComponentItem? {  //passed-in
         didSet {
-            if let notes = currentComponentItem?.cNotes {
-                currentNotes.text = notes
-            }
-            
-            if let website = currentComponentItem?.cWebsite {
-                currentWebsite = website
-            }
-            
-            if let media = currentComponentItem?.cMedia {
-                currentMedia = media
-            }
+            if let notes = currentComponentItem?.cNotes {currentNotes.text = notes}
+            if let website = currentComponentItem?.cWebsite {currentWebsite = website}
+            if let media = currentComponentItem?.cMedia {currentMedia = media}
         }
     }
     
     //MARK:- Local declared parameters loaded up from incoming variables
-
-        lazy var myScrollView: UIScrollView = {
-            let sv = UIScrollView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
-//            sv.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height * 3)
-            
-            sv.contentSize = CGSize(width: self.view.frame.size.width, height: 2000)
-            
-            sv.translatesAutoresizingMaskIntoConstraints = false
-            return sv
-        }()
-        
-        
+    lazy var myScrollView: UIScrollView = {
+        let v = UIScrollView()
+        v.backgroundColor = .cyan
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    
     var currentNotes: UITextView = {
-       var tempTextView = UITextView()
+        var tempTextView = UITextView()
         tempTextView.backgroundColor = UIColor.white
         tempTextView.textColor = UIColor.black
         tempTextView.font = UIFont.boldSystemFont(ofSize: 20)
         tempTextView.isEditable = false
         tempTextView.isScrollEnabled = false
-//        tempTextView.isScrollEnabled = true; tempTextView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+//                tempTextView.isScrollEnabled = true; tempTextView.heightAnchor.constraint(equalToConstant: 300).isActive = true
         tempTextView.translatesAutoresizingMaskIntoConstraints = false
         return tempTextView
     }()
-
+    
     var buttonWebsite: UIButton = {
-       var tempButton = UIButton()
+        var tempButton = UIButton()
         tempButton.setTitle("LOAD WEBSITE", for: .normal)
         tempButton.setTitleColor(UIColor.black, for: .normal)
         tempButton.backgroundColor = UIColor.white
         tempButton.translatesAutoresizingMaskIntoConstraints = false
         return tempButton
     }()
-
+    
     var currentWebsite: String?
-    
-    
-    
     var currentMedia: Data?
-    
-
-    
-    
-    
-    
-    
     
     //MARK:- Setup Navigation Bar
     private func setupNavigationBar(){
@@ -100,38 +68,42 @@ import AVFoundation
     }
     
     
+    private func useScrollableContraints(){
+        view.addSubview(myScrollView)
+        myScrollView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 5).isActive = true
+        myScrollView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -5).isActive = true
+        myScrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
+        myScrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -5).isActive = true
+        
+        [currentNotes, buttonWebsite].forEach{myScrollView.addSubview($0)}
+        currentNotes.topAnchor.constraint(equalTo: myScrollView.topAnchor, constant: 5).isActive = true
+        currentNotes.leftAnchor.constraint(equalTo: myScrollView.leftAnchor, constant: 5).isActive = true
+        currentNotes.rightAnchor.constraint(equalTo: myScrollView.rightAnchor, constant: -5).isActive = true
+    }
+    
+    private func noScrollViewConstraints(){
+        [currentNotes, buttonWebsite].forEach{view.addSubview($0)}
+        currentNotes.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive = true
+        currentNotes.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
+        currentNotes.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+//        currentNotes.heightAnchor.constraint(equalToConstant: 800).isActive = true
+    }
+    
     //MARK:- Swift Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
         view.backgroundColor = UIColor.lightGray
-        
-        view.addSubview(myScrollView)
-        
-
-
-        [currentNotes, buttonWebsite, scrollView].forEach{view.addSubview($0)}
-        
-        currentNotes.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-        currentNotes.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        currentNotes.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
-        currentNotes.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -10).isActive = true
-        
-        buttonWebsite.topAnchor.constraint(equalTo: currentNotes.bottomAnchor, constant: 30).isActive = true
-        buttonWebsite.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
-
         print("currentNotext.text --> \(currentNotes.text ?? "")")
         
         
+        [currentNotes].forEach{view.addSubview($0)}
+        currentNotes.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive = true
+        currentNotes.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
+        currentNotes.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
         
         
+//        noScrollViewConstraints()
+//        useScrollableContraints()
     }
-    
-    
-    
-    
-    
-    
-    
 }
