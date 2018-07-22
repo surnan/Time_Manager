6ea812d7649069091a0c32d853e0146aebaa603e
surnan <forsuresh@yandex.com>
2018-07-22 14:46:24 -0400

    added two additional buttons. One to show saved Image and another to show saved video.  Unfortunately, the saving part to document directory still isn't implemented

--- Time_Manager/Time_Manager/Extras/Wild_Stuff.swift
+++ Time_Manager/Time_Manager/Extras/Wild_Stuff.swift
@@ -12,10 +12,15 @@ import MobileCoreServices  // --> kUTTypeMovie
 class Wild_Stuff: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     
-    func printCoreDataPath(){
+    func getCurrentPath() -> URL {
         let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        print(urls[urls.count-1] as URL)
+        //        print(urls[urls.count-1] as URL)
+        
+        let temp = urls[urls.count-1] as URL
+        return temp
     }
+    
+    var currentPath : URL!
     
     var myLabel : UILabel = {
         var tempLabel = UILabel()
@@ -44,15 +49,32 @@ class Wild_Stuff: UIViewController, UIImagePickerControllerDelegate, UINavigatio
         return tempButton
     }()
     
+    var myShowPicButton: UIButton = {
+        var tempButton = UIButton()
+        tempButton.setTitleColor(UIColor.white, for: .normal)
+        tempButton.setTitle("Show Saved Picture", for: .normal)
+        tempButton.backgroundColor = UIColor.black
+        tempButton.translatesAutoresizingMaskIntoConstraints = false
+        return tempButton
+    }()
+    
+    var myShowVideoButton: UIButton = {
+        var tempButton = UIButton()
+        tempButton.setTitleColor(UIColor.white, for: .normal)
+        tempButton.setTitle("Show Saved Video", for: .normal)
+        tempButton.backgroundColor = UIColor.blue
+        tempButton.translatesAutoresizingMaskIntoConstraints = false
+        return tempButton
+    }()
     
     
     var myImagePickerController: UIImagePickerController = {
         let tempPicker = UIImagePickerController()
         tempPicker.sourceType = .photoLibrary
         tempPicker.allowsEditing = true
-//        if let allMedia = UIImagePickerController.availableMediaTypes(for: .photoLibrary) {
-//            tempPicker.mediaTypes = allMedia
-//        }
+        //        if let allMedia = UIImagePickerController.availableMediaTypes(for: .photoLibrary) {
+        //            tempPicker.mediaTypes = allMedia
+        //        }
         return tempPicker
     }()
     
@@ -62,57 +84,67 @@ class Wild_Stuff: UIViewController, UIImagePickerControllerDelegate, UINavigatio
         tempPicker.sourceType = .photoLibrary
         tempPicker.mediaTypes = [kUTTypeMovie as String]
         tempPicker.allowsEditing = false
-//        if let allMedia = UIImagePickerController.availableMediaTypes(for: .photoLibrary) {
-//            tempPicker.mediaTypes = allMedia
-//        }
+        //        if let allMedia = UIImagePickerController.availableMediaTypes(for: .photoLibrary) {
+        //            tempPicker.mediaTypes = allMedia
+        //        }
         return tempPicker
     }()
-
+    
     
     
     //MARK:- Button Action Code
     
     @objc func handleAlbumButtonPressed(){
-//        let videoPicker = UIImagePickerController()
-//        videoPicker.delegate = self
-//        videoPicker.sourceType = .photoLibrary  //enum [camera, savedPhotosAlbum ]
-////        videoPicker.mediaTypes = [kUTTypeMovie as String]
-//        self.present(videoPicker, animated: true, completion: nil)
-
+        //        let videoPicker = UIImagePickerController()
+        //        videoPicker.delegate = self
+        //        videoPicker.sourceType = .photoLibrary  //enum [camera, savedPhotosAlbum ]
+        ////        videoPicker.mediaTypes = [kUTTypeMovie as String]
+        //        self.present(videoPicker, animated: true, completion: nil)
+        
+        var fileManger = FileManager.default
+        
         self.present(myImagePickerController, animated: true, completion: nil)
     }
-
+    
     @objc func handleVideoButtonPressed(){
-//        let videoPicker = UIImagePickerController()
-//        videoPicker.delegate = self
-//        videoPicker.sourceType = .photoLibrary  //enum [camera, savedPhotosAlbum ]
-//        videoPicker.mediaTypes = [kUTTypeMovie as String]
-//        self.present(videoPicker, animated: true, completion: nil)
+        //        let videoPicker = UIImagePickerController()
+        //        videoPicker.delegate = self
+        //        videoPicker.sourceType = .photoLibrary  //enum [camera, savedPhotosAlbum ]
+        //        videoPicker.mediaTypes = [kUTTypeMovie as String]
+        //        self.present(videoPicker, animated: true, completion: nil)
         
         self.present(myImagePickerController2, animated: true, completion: nil)
         
     }
-
+    
+    @objc func handleShowPicButtonPressed(){
+        self.present(myImagePickerController, animated: true, completion: nil)
+        
+    }
+    @objc func handleShowVideoButtonPressed(){
+        self.present(myImagePickerController2, animated: true, completion: nil)
+        
+    }
+    
     
     
     //MARK:- Image Picker Functions
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
-
         if picker == myImagePickerController {
-            print("PICKER #1")
+            //            print("PICKER #1")
+            var imageURL: NSURL?
+            imageURL = info["UIImagePickerControllerReferenceURL"] as? NSURL
+            print("\n==\nimage URL = \(imageURL!)")
+            
         } else {
-            print("PICKER #2")
+            //            print("PICKER #2")
             
             var videoURL: NSURL?
             videoURL = info["UIImagePickerControllerReferenceURL"] as? NSURL
-            print("video URL = \(videoURL!)")
-            
+            print("\n==\nvideo URL = \(videoURL!)")
         }
-        
         //        iconImage.image = info[UIImagePickerControllerEditedImage] != nil ? info[UIImagePickerControllerEditedImage] as? UIImage :
-//            info[UIImagePickerControllerOriginalImage] as? UIImage
-        
-        
+        //            info[UIImagePickerControllerOriginalImage] as? UIImage
         dismiss(animated: true, completion: nil)
     }
     
@@ -130,21 +162,37 @@ class Wild_Stuff: UIViewController, UIImagePickerControllerDelegate, UINavigatio
         myImagePickerController.delegate = self
         myImagePickerController2.delegate = self
         
+        print("getCurrentPath = \(getCurrentPath())")
+        
+        
         myAlbumButton.addTarget(self, action: #selector(handleAlbumButtonPressed), for: .touchDown)
         myVideoButton.addTarget(self, action: #selector(handleVideoButtonPressed), for: .touchDown)
+        myShowPicButton.addTarget(self, action: #selector(handleShowPicButtonPressed), for: .touchDown)
+        myShowVideoButton.addTarget(self, action: #selector(handleShowVideoButtonPressed), for: .touchDown)
         
-        [myLabel, myAlbumButton, myVideoButton].forEach{view.addSubview($0)}
+        
+        
+        [myLabel, myAlbumButton, myVideoButton, myShowPicButton, myShowVideoButton].forEach{view.addSubview($0)}
         
         NSLayoutConstraint.activate([
             myLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            myLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            myAlbumButton.topAnchor.constraint(equalTo: myLabel.bottomAnchor, constant: 100),
+            myLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            
+            myAlbumButton.topAnchor.constraint(equalTo: myLabel.bottomAnchor  , constant: 70),
             myAlbumButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             myAlbumButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
-            myVideoButton.topAnchor.constraint(equalTo: myAlbumButton.bottomAnchor, constant: 100),
-            myVideoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            myVideoButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5)
             
+            myShowPicButton.topAnchor.constraint(equalTo: myAlbumButton.bottomAnchor, constant: 30),
+            myShowPicButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myShowPicButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            
+            myVideoButton.topAnchor.constraint(equalTo: myShowPicButton.bottomAnchor, constant: 70),
+            myVideoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myVideoButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            
+            myShowVideoButton.topAnchor.constraint(equalTo: myVideoButton.bottomAnchor, constant: 30),
+            myShowVideoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myShowVideoButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
             ])
     }
 }
